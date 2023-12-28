367354bcdbf64dad17997ecaf54ba07ac91d85df
Mezbah <mezbahuddinmaruf12345@gmail.com>
2023-12-28 22:06:43 +0600

    Solve 1915D - Unnatural Language Processing [900]

---
+++ contests/1915D - Unnatural Language Processing.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+
+
+void solve(){
+
+    int n; cin>>n;
+    string s; cin>>s;
+    // cout<<s[0]<<s[1];
+    for(int i=0; i<n; i++){
+        cout<<s[i];
+        if(i==n-1)break;
+        if(s[i]=='a' ||  s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+            if(s[i+2]!='a' && s[i+2]!='e' &&s[i+2]!='i' &&s[i+2]!='o' &&s[i+2]!='u'){
+                i++;
+                if(i!=n-1)cout<<s[i]<<".";
+                else cout<<s[i];
+            }else{
+                if(i!=n-1)cout<<".";
+            }
+        }
+    }
+    cout<<endl;
+
+
+    // int n; cin>>n;
+    // ll x;
+    // ll int sum=0;
+    // for(int i=0; i<n; i++){
+    //     cin>>x;
+    //     sum+=x;
+    // }
+    // if((long long int)sqrt(sum)*(long long int)sqrt(sum)==sum){
+    //     cout<<"yes"<<endl;
+    // }else{
+    //     cout<<"no"<<endl;
+    // }
+}
+
+
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+ 
+
