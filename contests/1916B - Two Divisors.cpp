650a73769f4e09c765df1d25b75e815ec97028c2
Mezbah <mezbahuddinmaruf12345@gmail.com>
2023-12-31 19:47:48 +0600

    Solve 1916B - Two Divisors [1000]

---
+++ contests/1916B - Two Divisors.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+ll gcd(ll a, ll b){
+    ll g=__gcd(a,b);
+    return g;
+}
+
+
+
+
+void solve(){
+    ll a,b;
+    cin>>a>>b;
+    if(a==1 || b==1){
+        cout<<a*a*b*b<<endl;
+    }else if(b%a==0){
+        cout<<b/gcd(a,b)*b<<endl;
+    }
+    else{
+        cout<<b*(a/gcd(a,b))<<endl;
+    }
+}
+
+
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+ 
+
