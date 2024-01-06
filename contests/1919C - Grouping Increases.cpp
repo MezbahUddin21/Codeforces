fa2229ae4673fac9ee8ccb076e44ebaf0e9cfd7c
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-01-07 04:03:37 +0600

    Solve 1919C - Grouping Increases [1400]

---
+++ contests/1919C - Grouping Increases.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+void solve(){
+   int n; cin>>n;
+   int a=1e6,b=1e6;
+   int k=0,ans=0;
+   while(n--){
+    cin>>k;
+    if(k<=a)a=k;
+    else if(k<=b)b=k;
+    else {a=k; ans++;}
+    if(a>b)swap(a,b);
+   }
+   cout<<ans<<endl;
+}
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+ 
+
+
+
