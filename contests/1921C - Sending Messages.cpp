a3d5134f8ddbdba9201c4e4c80298d1731828848
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-02-03 10:47:37 +0600

    Solve 1921C - Sending Messages [900]

---
+++ contests/1921C - Sending Messages.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+void solve(){
+    ll n,f,a,b; cin>>n>>f>>a>>b;
+    vector<int>v(n+1);
+    vector<int>vs(n+1);
+    for(int i=1; i<=n; i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    ll int l;
+    for(int i=1; i<=n; i++){
+            l=min((v[i]-v[i-1])*a,b);
+        f-=l;
+    }
+
+    if(f>0){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+
+}
+
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+ 
+
+
+
