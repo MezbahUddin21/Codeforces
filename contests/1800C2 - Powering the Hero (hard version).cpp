ee36b7894f6d5e34349a91f2976fe44a3bdc84df
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-02-04 11:01:12 +0600

    Solve 1800C2 - Powering the Hero (hard version) [1100]

---
+++ contests/1800C2 - Powering the Hero (hard version).cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+void solve(){
+    ll n,a; cin>>n;
+    ll sum=0;
+    multiset<int,greater<int>>ms;
+    for(ll i=0; i<n; i++){
+        cin>>a;
+        if(a==0){
+            if(ms.empty())continue;
+            else {
+                sum+=*ms.begin();
+                ms.erase(ms.begin());
+            }
+        }else{
+            ms.insert(a);
+        }
+    }
+    cout<<sum<<endl;
+    
+}
+
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+ 
+
+
+
