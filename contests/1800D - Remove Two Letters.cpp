a9a8fa0b68fc045f47d5a7096534c9d578c7cf16
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-02-04 13:06:43 +0600

    Solve 1800D - Remove Two Letters [1200]

---
+++ contests/1800D - Remove Two Letters.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+void solve(){
+    int n; cin>>n;
+    string s; cin>>s;
+    string s2;
+    int cnt=0;
+    for(int i=0; i<n-1; i++){
+    string s1;
+        s1.push_back(s[i]);
+        s1.push_back(s[i+1]);
+        if(s1!=s2){
+            swap(s1[0],s1[1]);
+            if(s1!=s2)cnt++;
+        }
+        s2=s1;
+    }
+
+    cout<<cnt<<endl;
+}
+
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+ 
+
+
+
