13023d709f93b81c1f2e42dfb20cab50276c19a4
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-02-18 12:38:25 +0600

    Solve 96B - Lucky Numbers (easy) [1300]

---
+++ contests/96B - Lucky Numbers (easy).cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+
+int cel(int a,float l){
+    return ceil(a/l);
+}
+
+ll findd(ll x, int c4, int c7,ll n)
+{
+    if (x >= 1e12) return 1e12;
+    if (c4 == c7 && c4 != 0 && x >=n ) {
+        return x;
+    }
+    return min(findd(x*10+4,c4+1,c7,n),findd(x*10+7,c4,c7+1,n));
+}
+
+
+void solve() {
+  ll n; cin>>n;
+    cout<<findd(0,0,0,n);
+  
+}
+// void solve(){
+//     string s; cin>>s;
+//     int n= s.size();
+//     string s1="";
+//     int cnt=0;
+//     for(int i=0; i<n; i++){
+//         s1+=s[i];
+//         if(s[i]=='1')cnt++;
+//         if(s[i]=='0') cnt=0;
+//         if(cnt==5)s1+='0';
+
+//     }
+//     cout<<s<<endl;
+//     cout<<"Stuffing: "<<s1<<endl;
+    
+//     string s2="";
+//     cnt=0;
+//     for(int i=0; i<s1.size(); i++){
+//         s2+=s1[i];
+//         if(s[i]=='1')cnt++;
+//         else cnt=0;
+//         if(cnt==5 and s1[i+1]=='0'){
+//             cnt=0; 
+//             i++;
+//         }
+
+
+//     } 
+//     cout<<"De-Stuffing: "<<s2<<endl;
+// }
+
+ 
+int main() {
+    mezbah();
+    // int t;
+    // cin>>t;
+    
+    // while(t--){
+        solve();
+    // }
+    return 0;
+}
+
+
+ 
+
+
+
