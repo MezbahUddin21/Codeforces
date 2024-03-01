419cd853f7c7f7f7fefeff04cc2c650ab5490efc
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-03-02 01:12:53 +0600

    Solve 1934A - Too Min Too Max [800]

---
+++ contests/1934A - Too Min Too Max.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+
+int cel(int a,float l){
+    return ceil(a/l);
+}
+
+
+
+void solve() {
+    int n; cin>>n;
+    vector<int>v(n);
+    for(auto &i:v)cin>>i;
+    sort(v.begin(),v.end());
+    int i=v[0],j=v[n-1],k=v[1],l=v[n-2];
+    cout<<abs(i-j)+abs(j-k)+abs(k-l)+abs(l-i)<<endl;
+
+}
+
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+ 
+
+
+
