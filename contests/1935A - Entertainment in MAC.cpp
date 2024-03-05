16a31812cd73f1906e691fcbff57091f46ca3d90
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-03-05 21:31:58 +0600

    Solve 1935A - Entertainment in MAC [800]

---
+++ contests/1935A - Entertainment in MAC.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+
+int cel(int a,float l){
+    return ceil(a/l);
+}
+
+void solve(){
+    ll int n; cin>>n;
+    string s,ss; cin>>s;
+    ss=s;
+    reverse(ss.begin(),ss.end());
+    int f=s.size();
+    int l=0;
+    for(int i=0; i<f/2; i++){
+        if(s[i]==s[f-i-1])continue;
+        else if(s[i]>s[f-i-1]){
+            l=1;
+        }else break;
+    }
+    if(l==0){
+        cout<<s<<endl;
+    }else cout<<ss<<s<<endl;
+
+}
+
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+ 
+
+
+
