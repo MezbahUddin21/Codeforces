cf6d09716d296dae7dbb406461cc2dbfb0e4fb5c
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-03-11 21:23:55 +0600

    Solve 1941C - Rudolf and the Ugly String [900]

---
+++ contests/1941C - Rudolf and the Ugly String.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sortv sort(v.begin(),v.end())
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+
+int cel(int a,float l){
+    return ceil(a/l);
+}
+void solve(){
+    ll n; cin>>n;
+    string s; cin>>s;
+    ll cnt=0;
+
+    for(int i=0; i<n-2; i++){
+        if(n-i>=4 and s[i]=='m' and s[i+1]=='a' and s[i+2]=='p'  and s[i+3]=='i' and s[i+4]=='e'){
+            cnt++;
+            i+=4;
+        }else if(s[i]=='m' and s[i+1]=='a' and s[i+2]=='p'){
+            cnt++;
+            i+=2;
+        }else if(s[i]=='p' and s[i+1]=='i' and s[i+2]=='e'){
+            cnt++;
+            i+=2;
+        }
+    }
+    cout<<cnt<<endl;
+}
+
+// void solve(){
+//     ll n; cin>>n;
+//     vector<int>v(n);
+//     ll sum=0;
+//     for(auto &i:v){cin>>i;sum+=i;}
+//     int cnt=0;
+//     if(sum==0){
+//         cout<<"yes"<<endl;
+//         return;
+//     }else if(n%2==0){
+//         cout<<"no"<<endl;
+//         return;
+//     }
+//     for(int i=1; i<n-1; i++){
+//         if(v[i-1]==0){
+//             continue;
+//         }else{
+//             v[i-1]-=1;
+//             v[i]-=2;
+
+//         }
+//     }
+//     cout<<cnt<<endl;
+
+
+// }
+
+// void solve(){
+//     ll n,k; cin>>n>>k;
+//     vector<pair<int,pair<int,int>>>v(n);
+//     int x;
+//     for(int i=0; i<n; i++){
+//         cin>>x;
+//         if(x%k!=0){
+//             v[i].first=x/k+1;
+//         }
+//         else{
+//             v[i].first=x/k;  
+//         }
+//         v[i].second.first=n-i;
+//         v[i].second.second=i+1;
+//     }
+//     sort(v.begin(),v.end(),greater<>());
+//     v[n-1].first-=1;
+//     for(int i=0; i<n; i++){
+//         cout<<v[i].first<<" ";
+//     }cout<<endl;
+//     for(int i=0; i<n; i++){
+//         cout<<v[i].second.second<<" ";
+//     }cout<<endl;
+
+// }
+
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+ 
+
+
+
