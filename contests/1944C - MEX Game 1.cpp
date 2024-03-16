70fcf75a5d41d347180ce0cc3eac2f9cabdb9740
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-03-16 22:27:35 +0600

    Solve 1944C - MEX Game 1 [1300]

---
+++ contests/1944C - MEX Game 1.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pyes cout<<"yes"<<"\n";
+#define pno cout<<"no"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+
+int cel(int a,float l){
+    return ceil(a/l);
+}
+
+void mine(){
+  int n;
+  cin >> n;
+  vector<int>v(n),vv(n + 1, 0);
+  forl(i,n)cin >> v[i];
+  forl(i,n)vv[v[i]]++;
+  int l = 0,r = 1;
+  n++;
+  forl(i,n){
+      if (vv[i] == 0){
+          l = i;
+          break;
+      }else if (vv[i] == 1){
+          if (r==1) r = 0;
+          else{
+              l = i;
+              break;
+          }
+      }
+  }
+  cout << l <<"\n";
+  }
+
+
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        mine();
+    }
+    return 0;
+}
+
+
+ 
+
+
+
