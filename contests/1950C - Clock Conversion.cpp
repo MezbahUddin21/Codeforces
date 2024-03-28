bf24e0fc144943852e74bfbb1e1d1eda5df0b553
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-03-28 21:30:01 +0600

    Solve 1950C - Clock Conversion [800]

---
+++ contests/1950C - Clock Conversion.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define mod 1000000007
+#define pyes cout<<"yes"<<"\n";
+#define pno cout<<"no"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void printt(vector<int> v){
+    cout<<v.size()<<endl;
+    for(int i=1; i<=v.size(); i++){
+        cout<<v[i]<<" ";
+    }cout<<endl;
+}
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+
+int cel(int a,float l){
+    return ceil(a/l);
+}
+ll max_subarr(vector <ll>v){
+    int n=v.size();
+    ll max_sum,curr_sum;
+    max_sum=curr_sum=0;
+    for(int i=0; i<n; i++){
+        curr_sum=max(curr_sum+v[i],v[i]);
+        max_sum=max(max_sum, curr_sum);
+    }   
+    return max_sum;
+}
+
+void mine(){
+    string s; cin>>s;
+    string s1;
+    s1+=s[0]; s1+=s[1];
+    string s2; s2+=s[3],s2+=s[4];
+    if(s1=="00"){
+        cout<<12<<":"<<s2<<" AM"<<endl;
+        return;
+    }
+    int m=stoi(s1);
+    if(s[0]=='0')cout<<s<<" "<<"AM"<<endl;
+    else{
+        if(m<12){
+            cout<<m%12<<":"<<s2<<" AM"<<endl;
+            return;
+        }
+        if(m%12>=10){
+            cout<<m%12<<":"<<s2<<" PM"<<endl;
+        }else{
+            if(m%12==0)cout<<12<<":"<<s2<<" PM"<<endl;
+            else cout<<0<<m%12<<":"<<s2<<" PM"<<endl;
+        }
+    }
+
+}
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        mine();
+    }
+    return 0;
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
