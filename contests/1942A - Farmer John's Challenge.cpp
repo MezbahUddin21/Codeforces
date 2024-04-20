f7491e2f60e1dfef3404872ede2cd80b4604f0ad
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-04-20 17:44:09 +0600

    Solve 1942A - Farmer John's Challenge [800]

---
+++ contests/1942A - Farmer John's Challenge.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define all(v) (v.begin(), v.end())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void printt(vector<int> v){
+    cout<<v.size()<<endl;
+    for(int i=1; i<=v.size(); i++){
+        cout<<v[i]<<" ";
+    }cout<<endl;
+}
+
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+
+int cel(int a,float l){
+    return ceil(a/l);
+}
+ll max_subarr(vector <ll>v){
+    int n=v.size();
+    ll max_sum,curr_sum;
+    max_sum=curr_sum=0;
+    for(int i=0; i<n; i++){
+        curr_sum=max(curr_sum+v[i],v[i]);
+        max_sum=max(max_sum, curr_sum);
+    }   
+    return max_sum;
+}
+
+ 
+
+
+void mine(){
+    int n; cin>>n;
+    int k; cin>>k;
+    if(n==2 and k==2){
+        cout<<1<<" "<<1<<endl;
+        return;
+    }else if(n==2 and k==1){
+        cout<<1<<" "<<2<<endl;
+        return;
+    }
+    if(k>1 and n!=k){
+        cout<<-1<<endl;
+        return;
+    }else{
+       if(n==k) {for(int i=1; i<=n; i++){
+            cout<<1<<" ";
+        }cout<<endl; return;}else{ for(int i=1; i<n; i++){
+            cout<<1<<" ";
+        }cout<<2<<endl;
+        return;}
+    }
+
+
+}
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        mine();
+    }
+    return 0;
+}
+
+
+ 
