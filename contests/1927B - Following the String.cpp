c3bb11f4ce47bc24021c2c8f71c2102f3a22e44d
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-07-15 15:48:28 +0600

    Improve 1927B - Following the String [900]

--- contests/1927B - Following the String.cpp
+++ contests/1927B - Following the String.cpp
@@ -1,43 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
 #define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
 #define setprecision(x) cout << fixed << setprecision(x)
-void mezbah(){
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-}
 
-
-void seive(int n){
-    vector<int>v(n+3);
-    for(int i=2; i*i<=n; i++){
-        if(v[i]==0){
-            for(int j=i*i; j<=n; j+=i){
-                v[j]=1;
-            }
-        }
-    }
-    for(int i=2; i<=n; i++){
-        if(v[i]==0)cout<<i<<" ";
-    }
-    cout<<endl;
+void printt(vector<int> v){
+    cout<<v.size()<<endl;
+    for(int i=1; i<=v.size(); i++){
+        cout<<v[i]<<" ";
+    }cout<<endl;
 }
+// set<int>primes;
+// void seive(int n){
+//     set<int>s;
+//     vector<int>v(n+3);
+//     for(int i=2; i*i<=n; i++){
+//         if(v[i]==0){
+//             for(int j=i*i; j<=n; j+=i){
+//                 v[j]=1;
+//             }
+//         }
+//     }
+//     for(int i=2; i<=n; i++){
+//         if(v[i]==0)s.insert(v[i]);
+//     }
+// }
 
 vector<ll> mPrimeFact(ll x) {
-	vector<ll> fact;
-	for(ll i = 2; i * i <= x; i++) {
-		ll cnt = 0; 
-		while(x % i == 0) {
-			cnt++;
-			x /= i;
-			fact.push_back(i);
-		}
-	}
-	if(x > 1) fact.push_back(x);
-	return fact;
+vector<ll> fact;
+for(ll i = 2; i * i <= x; i++) {
+ll cnt = 0;
+while(x % i == 0) {
+cnt++;
+x /= i;
+fact.push_back(i);
+}
+}
+    if(x > 1) fact.push_back(x);
+    return fact;
 }
 
 ll lcm(ll a,ll b){
@@ -46,46 +57,83 @@ ll lcm(ll a,ll b){
 }
 
 
-void solve(){
-    int n,count=0; cin>>n;
-    map<int,vector<char>>mp;
-    string ss="";
-    vector<int>v(n);
-    
-    for(auto &i:v)cin>>i;
 
+int cel(int a,float l){
+    return ceil(a/l);
+}
+ll max_subarr(vector <ll>v){
+    int n=v.size();
+    ll max_sum,curr_sum;
+    max_sum=curr_sum=0;
     for(int i=0; i<n; i++){
-        if(mp[v[i]].size()==0){
-                ss+='a';
-            mp[v[i]].push_back('a');
+        curr_sum=max(curr_sum+v[i],v[i]);
+        max_sum=max(max_sum, curr_sum);
+    }  
+    return max_sum;
+}
+int mod_add(int a, int b, int m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+int mod_mul(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+int mod_sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
 
-        }else{
-            mp[v[i]].push_back(mp[v[i]][mp[v[i]].size()-1]+1);
-            int l=4;
-            ss+=mp[v[i]][mp[v[i]].size()-1];
+vector<int> primes;
+ 
+void sieve() {
+    vector<bool> isPrime(1e5 + 5, true);
+    isPrime[0] = isPrime[1] = false;
+ 
+    for (int i = 2; i < 1e5 + 5; ++i) {
+        if (isPrime[i]) {
+            primes.push_back(i);
+            for (int j = i + i; j < 1e5 + 5; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
+
+ll cnt[33];
+
+void call(ll x){
+    forl(i,33){
+        ll p=1LL<<i;
+        ll c=x/p;
+        cnt[i]+=(c/2)*p;
+        if(c & 1LL){
+            cnt[i]+=x%p;
         }
 
     }
-    cout<<ss<<endl;
-
-
-
 }
 
+void mine(){
+    int n; cin>>n;
+    vector<int>v(n);
+    forl(i,n)cin>>v[i];
+
+    map<char,int>c;
+    for(int i=0; i<26; i++){
+        c['a'+i]=0;
+    }
+
+    forl(i,n){
+        for(auto &it:c){
+            if(it.second==v[i]){
+                cout<<it.first;
+                it.second++;
+                break;
+            }
+        }
+    }cout<<endl;
+    
+}
  
 int main() {
-    mezbah();
     int t;
     cin>>t;
-    
+   
     while(t--){
-        solve();
+        mine();
     }
     return 0;
 }
-
-
- 
-
-
 
