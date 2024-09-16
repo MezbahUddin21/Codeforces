3c43866bdff9f177798021fd1210854fa36550b1
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-09-16 16:41:35 +0600

    Solve 2000E - Photoshoot for Gorillas [1400]

---
+++ contests/2000E - Photoshoot for Gorillas.cpp
@@ -0,0 +1,175 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+
+void printt(vector<int> v){
+    cout<<v.size()<<endl;
+    for(int i=1; i<=v.size(); i++){
+        cout<<v[i]<<" ";
+    }cout<<endl;
+}
+// set<int>primes;
+// void seive(int n){
+//     set<int>s;
+//     vector<int>v(n+3);
+//     for(int i=2; i*i<=n; i++){
+//         if(v[i]==0){
+//             for(int j=i*i; j<=n; j+=i){
+//                 v[j]=1;
+//             }
+//         }
+//     }
+//     for(int i=2; i<=n; i++){
+//         if(v[i]==0)s.insert(v[i]);
+//     }
+// }
+
+vector<ll> mPrimeFact(ll x) {
+vector<ll> fact;
+for(ll i = 2; i * i <= x; i++) {
+ll cnt = 0;
+while(x % i == 0) {
+cnt++;
+x /= i;
+fact.push_back(i);
+}
+}
+    if(x > 1) fact.push_back(x);
+    return fact;
+}
+
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+
+
+
+int cel(int a,float l){
+    return ceil(a/l);
+}
+ll max_subarr(vector <ll>v){
+    int n=v.size();
+    ll max_sum,curr_sum;
+    max_sum=curr_sum=0;
+    for(int i=0; i<n; i++){
+        curr_sum=max(curr_sum+v[i],v[i]);
+        max_sum=max(max_sum, curr_sum);
+    }  
+    return max_sum;
+}
+int mod_add(int a, int b, int m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+int mod_mul(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+int mod_sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+
+vector<int> primes;
+ 
+void sieve() {
+    vector<bool> isPrime(1e5 + 5, true);
+    isPrime[0] = isPrime[1] = false;
+ 
+    for (int i = 2; i < 1e5 + 5; ++i) {
+        if (isPrime[i]) {
+            primes.push_back(i);
+            for (int j = i + i; j < 1e5 + 5; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
+
+ll cnt[33];
+
+void call(ll x){
+    forl(i,33){
+        ll p=1LL<<i;
+        ll c=x/p;
+        cnt[i]+=(c/2)*p;
+        if(c & 1LL){
+            cnt[i]+=x%p;
+        }
+
+    }
+}
+
+int bTd(vector<int> bi){
+    int dc=0;
+    for(auto i: bi){
+        dc=dc*2+i;
+    }
+    return dc;
+}
+
+vector<int> dTb(int n){
+    vector<int>bi;
+    while(n>0){
+        bi.push_back(n&1);
+        n=n>>1;
+    }
+    bi.push_back(0);
+    reverse all(bi);
+    return bi;
+
+}
+
+void mine(){
+
+    ll n,m,k; cin>>n>>m>>k;
+    ll sz; cin>>sz;
+    vector<ll>v(sz);
+    forl(i,sz) cin>>v[i];
+    sort allr(v);
+    vector<vector<ll>> grid(n,vector<ll>(m,0));
+    for(ll i=0;i<=n-k;i++){
+        for(ll j=0;j<=m-k;j++){
+            grid[i][j]++;
+            if(i+k<n) grid[i+k][j]--;
+            if(j+k<m) grid[i][j+k]--;
+            if(i+k<n and j+k<m) grid[i+k][j+k]++;
+        }
+    }
+    for(ll i=1;i<n;i++) grid[i][0]+=grid[i-1][0];
+    for(ll j=1;j<m;j++) grid[0][j]+=grid[0][j-1];
+    for(ll i=1;i<n;i++){
+        for(ll j=1;j<m;j++){
+            grid[i][j]+=grid[i-1][j]+grid[i][j-1]-grid[i-1][j-1];
+        }
+    }
+    vector<ll>vals;
+    for(ll i=0;i<n;i++){
+        for(ll j=0;j<m;j++){
+            vals.pb(grid[i][j]);
+        }
+    }
+    sort allr(vals);
+    ll ans =0 ;
+    for(ll i=0;i<min(sz,n*m);i++){
+        ans+=vals[i]*v[i];
+    }
+    cout<<ans<<endl;
+}
+
+
+ 
+int main() {
+    int t;
+    cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
