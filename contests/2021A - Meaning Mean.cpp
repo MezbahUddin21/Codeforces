b1696554de3b9dcbe37db824ef22e762b9956538
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-10-21 21:42:48 +0600

    Improve 2021A - Meaning Mean [800]

--- contests/2021A - Meaning Mean.cpp
+++ contests/2021A - Meaning Mean.cpp
@@ -1,49 +1,56 @@
+/* GREEN UNIVERSITY OF BANGLADESH
+    Md DinIslam, Batch-221 (CSE)
+*/
 #include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define space " "
-#define mod 1000000007
-#define pb push_back
-#define all(v) (v.begin(), v.end())
-#define allr(v) (v.begin(), v.end(),greater<>())
-#define pyes cout<<"YES"<<"\n";
-#define pno cout<<"NO"<<"\n";
-#define sortv sort(v.begin(),v.end())
-#define sortvg sort(v.begin(),v.end(),greater<>())
+
+// Debug..
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
 #define ll long long
-#define forl(i,n) for(int i=0;i<n;i++)
-#define for1(i,n) for(int i=1;i<=n;i++)
-#define setprecision(x) cout << fixed << setprecision(x)
+#define all(x) x.begin(), x.end()
+#define pb push_back
 
+#define GET_MACRO(_1, _2, _3, NAME, ...) NAME
+#define rep(...) GET_MACRO(__VA_ARGS__, rep2, rep1)(__VA_ARGS__)
+#define rep1(i, n) for(int i = 0; i < int(n); ++i)
+#define rep2(i, a, n) for(int i = int(a); i <= int(n); ++i)
+#define sz(x) int(x.size())
 
+void Din() {
+    int n;
+    cin >> n;
 
-
-void mezbah(){
-    int n; cin>>n;
-    vector<int>v(n);
-    forl(i,n)cin>>v[i];
-
-    sort all(v);
-    ll ans=v[0];
-
-    for(int i=1; i<n; i++){
-        ans=(ans+v[i])/2;
+    multiset<int> st;
+    rep(i, n) {
+        int x;
+        cin >> x;
+        st.insert(x);
     }
 
-    cout<<ans<<endl;
+    debug(st);
+    while (sz(st) > 1) {
+        auto x = st.begin();
+        st.erase(x);
+        auto y = st.begin();
+        st.erase(y);
+        
+        st.insert((*x + *y) / 2);
+    }
+
+    cout << *st.begin() << "\n";
 }
-
-
-
- 
 int main() {
-    int t;
-    cin>>t;
-   
-    while(t--){
-        mezbah();
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) { // Kickstart
+        // cout << "Case #" << i << ": ";
+        Din();
     }
     return 0;
 }
-
-
