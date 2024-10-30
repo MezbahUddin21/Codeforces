b10f8dcef079e540ff1ada8bd4ff236eb1fd24a9
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-10-30 08:05:06 +0600

    Solve 2035A - Sliding [800]

---
+++ contests/2035A - Sliding.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+
+
+
+
+void mine(){
+    ll n,m,r,c;
+    cin>>n>>m>>r>>c;
+
+    ll ans=0;
+    ans+=m-c;
+    ans+=(m+m-1)*(n-r);
+
+
+
+
+    cout<<ans<<endl;
+    
+}
+
+
+
+ 
+signed main() {
+    int t; 
+    cin>>t;
+
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
+
+
