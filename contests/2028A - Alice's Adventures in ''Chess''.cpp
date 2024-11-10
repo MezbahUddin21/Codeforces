f5cbf6d4376c3980324bd10cbc4a1ab4681bb487
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-11-11 00:41:28 +0600

    Solve 2028A - Alice's Adventures in ''Chess'' [900]

---
+++ contests/2028A - Alice's Adventures in ''Chess''.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+
+
+
+
+void mine(){
+    int n,a,b; cin>>n>>a>>b;
+
+    string s; cin>>s;
+
+    int x=0,y=0,f=41;
+    while(f--)for(int i=0; i<n; i++){
+        
+        if(s[i]=='N')y++;
+        else if(s[i]=='E')x++;
+        else if(s[i]=='S')y--;
+        else if(s[i]=='W')x--;
+
+        if(x==a and y==b){
+            pyes
+            return;
+        }
+
+    }
+    // cout<<x<<" "<<y<<endl;
+
+    pno
+}
+
+
+
+
+ 
+signed main() {
+    ios::sync_with_stdio(0),cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+
+//    int c=1;
+    while(t--){
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
+
+
