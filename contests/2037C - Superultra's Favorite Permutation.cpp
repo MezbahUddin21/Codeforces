f360708e9742c5749260e604114eb9280d5f6ab9
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-11-17 21:33:39 +0600

    Solve 2037C - Superultra's Favorite Permutation [1000]

---
+++ contests/2037C - Superultra's Favorite Permutation.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+
+
+void printt(vector<int> v){
+    cout<<v.size()<<endl;
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }cout<<endl;
+}
+
+
+void mine(){
+    int n; cin>>n;
+
+    if(n<5){
+        cout<<-1<<endl;
+    }else{
+        for(int i=1; i<=n; i+=2){
+            if(i==5)continue;
+            cout<<i<<space;
+        }
+        cout<<5<<space<<4<<space;
+
+        for(int i=2; i<=n; i+=2){
+            if(i==4)continue;
+            cout<<i<<space;
+        }
+        cout<<endl;
+    }
+
+}
+ 
+
+
+
+ 
+signed main() {
+    ios::sync_with_stdio(0),cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+
+//    int c=1;
+    while(t--){
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
+
