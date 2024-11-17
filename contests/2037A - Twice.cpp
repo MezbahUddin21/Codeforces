546231f25e86b2c65378bcce36ab2fe4a9ab879c
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-11-17 20:46:47 +0600

    Solve 2037A - Twice [800]

---
+++ contests/2037A - Twice.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+
+
+void printt(vector<int> v){
+    cout<<v.size()<<endl;
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }cout<<endl;
+}
+
+
+
+void mine(){
+    int n; cin>>n;
+    vector<int>v(n);
+
+    map<int,int>mp;
+
+    forl(i,n){
+        int x; cin>>x;
+        mp[x]++;
+    }
+
+    int cnt=0;
+    for(auto it:mp){
+        cnt+=it.second/2;
+    }
+
+    cout<<cnt<<endl;
+}
+
+
+
+
+ 
+signed main() {
+    ios::sync_with_stdio(0),cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+
+//    int c=1;
+    while(t--){
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
+
