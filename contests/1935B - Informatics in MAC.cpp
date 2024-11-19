54b8ab25f263c708dba37f0cd929730e079f81df
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-11-19 11:38:22 +0600

    Improve 1935B - Informatics in MAC [1200]

--- contests/1935B - Informatics in MAC.cpp
+++ contests/1935B - Informatics in MAC.cpp
@@ -14,159 +14,84 @@ using namespace std;
 #define forl(i,n) for(int i=0;i<n;i++)
 #define for1(i,n) for(int i=1;i<=n;i++)
 #define setprecision(x) cout << fixed << setprecision(x)
-void mezbah(){
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-}
+
 
 void printt(vector<int> v){
     cout<<v.size()<<endl;
-    for(int i=1; i<=v.size(); i++){
+    for(int i=0; i<v.size(); i++){
         cout<<v[i]<<" ";
     }cout<<endl;
 }
-// set<int>primes;
-// void seive(int n){
-//     set<int>s;
-//     vector<int>v(n+3);
-//     for(int i=2; i*i<=n; i++){
-//         if(v[i]==0){
-//             for(int j=i*i; j<=n; j+=i){
-//                 v[j]=1;
-//             }
-//         }
-//     }
-//     for(int i=2; i<=n; i++){
-//         if(v[i]==0)s.insert(v[i]);
-//     }
-// }
 
-vector<ll> mPrimeFact(ll x) {
-vector<ll> fact;
-for(ll i = 2; i * i <= x; i++) {
-ll cnt = 0;
-while(x % i == 0) {
-cnt++;
-x /= i;
-fact.push_back(i);
-}
-}
-    if(x > 1) fact.push_back(x);
-    return fact;
-}
-
-ll lcm(ll a,ll b){
-    ll g=__gcd(a,b);
-    return (a*b/g);
-}
-
-
-
-int cel(int a,float l){
-    return ceil(a/l);
-}
-ll max_subarr(vector <ll>v){
-    int n=v.size();
-    ll max_sum,curr_sum;
-    max_sum=curr_sum=0;
-    for(int i=0; i<n; i++){
-        curr_sum=max(curr_sum+v[i],v[i]);
-        max_sum=max(max_sum, curr_sum);
-    }  
-    return max_sum;
-}
-int mod_add(int a, int b, int m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-int mod_mul(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-int mod_sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-
-vector<int> primes;
- 
-void sieve() {
-    vector<bool> isPrime(1e5 + 5, true);
-    isPrime[0] = isPrime[1] = false;
- 
-    for (int i = 2; i < 1e5 + 5; ++i) {
-        if (isPrime[i]) {
-            primes.push_back(i);
-            for (int j = i + i; j < 1e5 + 5; j += i) {
-                isPrime[j] = false;
-            }
-        }
-    }
-}
-
-ll cnt[33];
-
-void call(ll x){
-    forl(i,33){
-        ll p=1LL<<i;
-        ll c=x/p;
-        cnt[i]+=(c/2)*p;
-        if(c & 1LL){
-            cnt[i]+=x%p;
-        }
-
-    }
-}
 
 void mine(){
-ll n;
-    cin>>n;
-    vector<ll> v(n);
-    forl(i,n)cin>>v[i];
-    unordered_map<ll,ll>mp;
-    for(int i=0;i<n;i++){
-        mp[v[i]]=1;
+    int n; cin>>n;
+
+    vector<int>v(n);
+    set<int>c;
+
+    forl(i,n){
+        cin>>v[i];
+        c.insert(v[i]);
     }
-    ll mex=0;
-    for(int i=0;i<1e5+5;i++){
-        if(mp[i]==0){
-            mex=i;
+
+    int mex=0;
+
+    int i=0;
+    for(auto it:c){
+        if(i==it){
+            mex=it+1;
+        }else break;
+
+        i++;
+    }
+
+
+    set<int>s;
+    int cnt=1;
+    int h=0;
+
+    forl(i,n){
+        if(v[i]<mex)s.insert(v[i]);
+        if(s.size()==mex){
+            h=i+1;
             break;
         }
     }
-    if(mex==0){
-        cout<<"2\n";
-        cout<<"1 1\n";
-        cout<<"2 "<<n<<"\n";
-        return;
-    }
-    mp.clear();
-    ll left=0;
-    vector<pair<ll,ll>>ans;
-    for(int i=0;i<n;i++){
-        if(v[i]<mex){
-            mp[v[i]]=1;
-        }
-        if(mp.size()==mex){
-            ans.pb({left+1,i+1});
-            left=i+1;
-            mp.clear();
+
+    s.clear();
+
+    for(int i=h; i<n; i++){
+        if(v[i]<mex)s.insert(v[i]);
+        if(s.size()==mex){
+            cnt++;
+            break;
         }
     }
-    ans[ans.size()-1].second=n;
-    if(ans.size()<2){
-        cout<<"-1\n";
-    }
-    else{
-        cout<<ans.size()<<"\n";
-        for(int i=0;i<ans.size();i++){
-            cout<<ans[i].first<<" "<<ans[i].second<<"\n";
-        }
-    }
+
+    if(cnt>1){
+        cout<<2<<endl;
+        cout<<1<<space<<h<<endl;
+        cout<<h+1<<space<<n<<endl;
+    }else cout<<-1<<endl;
+
     
 }
  
-int main() {
-    // mezbah();
-    int t;
+
+
+
+ 
+signed main() {
+    ios::sync_with_stdio(0),cin.tie(0); cout.tie(0);
+    int t=1;
     cin>>t;
-   
+
+//    int c=1;
     while(t--){
+        // cout<<"Case "<<c<<": "; c++;
         mine();
     }
     return 0;
 }
+
