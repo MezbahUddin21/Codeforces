3fb6b55739326a5a4b533a15b5864fe381603823
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-11-19 10:56:15 +0600

    Improve 1935A - Entertainment in MAC [800]

--- contests/1935A - Entertainment in MAC.cpp
+++ contests/1935A - Entertainment in MAC.cpp
@@ -1,89 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
 #define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
 #define setprecision(x) cout << fixed << setprecision(x)
-void mezbah(){
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
+
+
+void printt(vector<int> v){
+    cout<<v.size()<<endl;
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }cout<<endl;
 }
 
 
-void seive(int n){
-    vector<int>v(n+3);
-    for(int i=2; i*i<=n; i++){
-        if(v[i]==0){
-            for(int j=i*i; j<=n; j+=i){
-                v[j]=1;
-            }
-        }
-    }
-    for(int i=2; i<=n; i++){
-        if(v[i]==0)cout<<i<<" ";
-    }
-    cout<<endl;
+void mine(){
+    int n; cin>>n;
+    string s; cin>>s;
+
+    string f=s;
+
+    reverse(f.begin(),f.end());
+
+    if(f<s)cout<<f+s<<endl;
+    else cout<<s<<endl;
 }
-
-vector<ll> mPrimeFact(ll x) {
-	vector<ll> fact;
-	for(ll i = 2; i * i <= x; i++) {
-		ll cnt = 0; 
-		while(x % i == 0) {
-			cnt++;
-			x /= i;
-			fact.push_back(i);
-		}
-	}
-	if(x > 1) fact.push_back(x);
-	return fact;
-}
-
-ll lcm(ll a,ll b){
-    ll g=__gcd(a,b);
-    return (a*b/g);
-}
+ 
 
 
-
-int cel(int a,float l){
-    return ceil(a/l);
-}
-
-void solve(){
-    ll int n; cin>>n;
-    string s,ss; cin>>s;
-    ss=s;
-    reverse(ss.begin(),ss.end());
-    int f=s.size();
-    int l=0;
-    for(int i=0; i<f/2; i++){
-        if(s[i]==s[f-i-1])continue;
-        else if(s[i]>s[f-i-1]){
-            l=1;
-        }else break;
-    }
-    if(l==0){
-        cout<<s<<endl;
-    }else cout<<ss<<s<<endl;
-
-}
 
  
-int main() {
-    mezbah();
-    int t;
+signed main() {
+    ios::sync_with_stdio(0),cin.tie(0); cout.tie(0);
+    int t=1;
     cin>>t;
-    
+
+//    int c=1;
     while(t--){
-        solve();
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
     }
     return 0;
 }
-
-
- 
-
-
 
