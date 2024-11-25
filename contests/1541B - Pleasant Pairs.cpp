a78710c27baad37f33b4e227902ec492c147a203
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-11-25 19:33:58 +0600

    Solve 1541B - Pleasant Pairs [1200]

---
+++ contests/1541B - Pleasant Pairs.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+void mine(){
+    ll n; cin>>n;
+
+    pair<ll,ll>v[n];
+
+    forl(i,n){
+        cin>>v[i].first;
+        v[i].second=i+1;
+    }
+
+
+    sort(v,v+n);
+
+    ll ans=0;
+
+    for(ll i=0; i<n-1; i++){
+        for(ll j=i+1; j<n; j++){
+            if((v[i].first*v[j].first)>2*n)break;
+
+            if(v[i].first*v[j].first==v[i].second+v[j].second)ans++;
+        }
+    }
+
+    cout<<ans<<endl;
+
+}
+
+
+ 
+int main() {
+    fast;
+    int t=1;
+    cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
