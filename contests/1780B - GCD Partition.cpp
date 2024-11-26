d7c31a1d2a63e09441a81b6eaaea9c1b92f9e361
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-11-27 00:04:21 +0600

    Solve 1780B - GCD Partition [1100]

---
+++ contests/1780B - GCD Partition.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"Yes"<<"\n";
+#define pno cout<<"No"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+void mine(){
+    int n; cin>>n;
+
+    vector<ll>v(n);
+    ll sum=0;
+
+    forl(i,n){cin>>v[i];sum+=v[i];}
+
+    ll sum1=0,sum2=0;
+
+    ll ans=0;
+
+    for(int i=0; i<n-1; i++){
+        sum1+=v[i];
+        sum-=v[i];
+
+        ans=max(ans,__gcd(sum1,sum));
+
+    }
+
+
+    cout<<ans<<endl;
+}
+
+
+ 
+int main() {
+    fast;
+    int t=1;
+    cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
