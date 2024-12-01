2381d065e0a25702d34ac56cde11599a9bd8b023
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-12-01 08:30:56 +0600

    Solve 2034A - King Keykhosrow's Mystery [800]

---
+++ contests/2034A - King Keykhosrow's Mystery.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast {ios_base::sync_with_stdio(false); cin.tie(0);}
+
+ll lcm(ll a, ll b) {
+    ll g = __gcd(a, b);
+    return (a * b / g);
+}
+
+
+
+void mine(){
+    int a,b; cin>>a>>b;
+    cout<<lcm(a,b)<<endl;
+}
+
+
+
+ 
+int main() {
+    fast
+    int t=1;
+    cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
+
+
