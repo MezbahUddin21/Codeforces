650fa8b3d52add586096779bf342ef3513c1cf4e
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-12-01 08:44:52 +0600

    Solve 2034B - Rakhsh's Revival [1000]

---
+++ contests/2034B - Rakhsh's Revival.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast {ios_base::sync_with_stdio(false); cin.tie(0);}
+
+
+void mine(){
+    int n,m,k; cin>>n>>m>>k;
+
+    string s; cin>>s;
+
+    int cnt=0,ans=0;
+    for(int i=0; i<n; i++){
+        if(s[i]=='0'){
+            cnt++;
+            if(cnt>=m){
+                ans++;
+                cnt=0;
+                i+=k-1;
+            }
+        }else cnt=0;
+    }
+
+    cout<<ans<<endl;
+
+
+}
+
+
+
+ 
+int main() {
+    fast
+    int t=1;
+    cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
+
+
