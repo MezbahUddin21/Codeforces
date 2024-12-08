fc021ceb338cd3894647c0905b605cae53c75fc4
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-12-08 08:08:03 +0600

    Solve 1581B - Diameter of Graph [1200]

---
+++ contests/1581B - Diameter of Graph.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast {ios_base::sync_with_stdio(false); cin.tie(0);}
+
+
+void mine(){
+    
+    ll n,m,k;
+
+    cin>>n>>m>>k;
+
+    ll ed=(ll)n*(n-1)/2;
+
+    k-=2;
+    if(m<n-1 or m>ed)pno
+
+    else if(k<=0){
+
+        if(n==1 and k==0)pyes
+        else pno
+
+    }else if(n<=2)pyes
+    else if(k>=2 or m==ed)pyes
+    else pno
+
+
+
+
+}
+
+
+
+ 
+int main() {
+    fast
+    int t=1;
+    cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
+
+
