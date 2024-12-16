04e5f0834f0e8d2b42c3ae64fbc21df57eacf023
Mezbah <mezbahuddinmaruf12345@gmail.com>
2024-12-16 10:50:17 +0600

    Solve 2044D - Harder Problem [1100]

---
+++ contests/2044D - Harder Problem.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast {ios_base::sync_with_stdio(false); cin.tie(0);}
+
+
+void mine(){
+
+    int n; cin>>n;
+    vector<int>v(n);
+
+    forl(i,n)cin>>v[i];
+
+    set<int>s;
+
+    for1(i,n){
+        s.insert(i);
+    }
+
+    forl(i,n){
+        if(s.find(v[i])!=s.end()){
+            cout<<v[i]<<space;
+            s.erase(v[i]);
+        }else{
+            cout<<*s.begin()<<space;
+            s.erase(s.begin());
+        }
+    }cout<<endl;
+    
+    
+    
+}
+
+
+
+ 
+int main() {
+    fast
+    int t=1;
+    cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
+
+
