4effa9cb8735d8446ed1d1a7f25cc8107c7b548a
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-01-09 23:27:58 +0600

    Solve 1698B - Rising Sand [800]

---
+++ contests/1698B - Rising Sand.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin()a, v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+ 
+ 
+ 
+void mine(){
+    
+
+    int n,k; cin>>n>>k;
+
+    vector<int>v(n);
+
+    forl(i,n)cin>>v[i];
+
+
+    if(k==1){
+        cout<<(n-1)/2<<endl;
+        return;
+    }
+
+    int ans=0;
+
+    for(int i=1; i<n-1; i++){
+        if(v[i]>v[i-1]+v[i+1])ans++;
+    }
+    
+
+    cout<<ans<<endl;
+ 
+ 
+}
+ 
+ 
+ 
+ 
+signed main() {
+    int t=1;
+ 
+    cin>>t;
+ 
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
