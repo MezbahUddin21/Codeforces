250c858b9570c3c8db44a50408db5d3d0e743db3
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-01-09 23:10:07 +0600

    Solve 1698A - XOR Mixup [800]

---
+++ contests/1698A - XOR Mixup.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin()a, v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+ 
+ 
+ 
+void mine(){
+    int n; cin>>n;
+
+    vector<int>v(n);
+
+    forl(i,n)cin>>v[i];
+
+    cout<<v[0]<<endl;
+    
+ 
+ 
+}
+ 
+ 
+ 
+ 
+signed main() {
+    int t=1;
+ 
+    cin>>t;
+ 
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
