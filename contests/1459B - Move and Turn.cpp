45da7a4e59b391027f7f56ce0de606f8a9d700af
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-01-12 15:46:46 +0600

    Solve 1459B - Move and Turn [1300]

---
+++ contests/1459B - Move and Turn.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin()a, v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+ 
+ 
+ 
+void mine(){
+
+    int n; cin>>n;
+
+    if(n%2==0)cout<<((n/2+1)*(n/2+1));
+    else cout<<2*(n/2+1)*(n/2+2);
+}
+ 
+ 
+ 
+ 
+signed main() {
+    int t=1;
+ 
+    // cin>>t;
+ 
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
