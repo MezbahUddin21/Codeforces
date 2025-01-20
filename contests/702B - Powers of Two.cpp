1d034546bf022ca4e095c3664f666566c8c79433
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-01-20 14:38:37 +0600

    Solve 702B - Powers of Two [1500]

---
+++ contests/702B - Powers of Two.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define int long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+
+void mine() {
+    int n; cin>>n;
+
+
+    map<int,int>mp;
+
+    int ans=0;
+
+    forl(i,n){
+        int x; cin>>x;
+        
+        for(int j=30; j>=0; j--){
+            if((1<<j)<x)break;
+            ans+=mp[(1<<j)-x];
+        }
+
+        mp[x]++;
+
+    }
+
+    cout<<ans<<endl;
+
+}
+
+ 
+signed main() {
+    fast;
+    int t=1;
+    // cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
