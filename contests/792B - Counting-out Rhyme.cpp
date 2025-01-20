afcaf701da5c1d74537c2fac328596c409b1f3d0
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-01-20 19:21:11 +0600

    Solve 792B - Counting-out Rhyme [1300]

---
+++ contests/792B - Counting-out Rhyme.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define int long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+
+void mine() {
+    int n,k; cin>>n>>k;
+
+    vector<int>v(n);
+
+    iota(v.begin(),v.end(),1);
+
+    int idx=0;
+
+    forl(i,k){
+        int x; cin>>x;
+
+        idx+=x;
+
+        idx%=v.size();
+
+        cout<<*(v.begin()+idx)<<space;
+
+        v.erase(v.begin()+idx);
+
+    }
+
+}
+
+ 
+signed main() {
+    fast;
+    int t=1;
+    // cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
