4866d0160e3e0c6bfb5848cc37e9fcb5ebe60fb5
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-02-21 17:54:00 +0600

    Solve 2064A - Brogramming Contest [800]

---
+++ contests/2064A - Brogramming Contest.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+
+
+
+void mine(){
+    int n; cin>>n;
+
+    string s; cin>>s;
+
+    s='0'+s;
+
+    int cnt=0;
+
+    for1(i,n){
+        if(s[i]==0)continue;
+        else{
+            for(int j=i; j<=n; j++){
+                if(s[j]!=s[j-1])cnt++;
+            }
+
+            break;
+        }
+    }
+
+    cout<<cnt<<endl;
+
+    
+
+}
+
+
+ 
+int main() {
+    int t=1;
+    cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
