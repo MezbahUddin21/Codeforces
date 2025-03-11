1f2a0e6ffd8f19c4ee25cd5eaedd39dc6d995776
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-03-11 23:14:31 +0600

    Solve 2078A - Final Verdict [800]

---
+++ contests/2078A - Final Verdict.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Debug..
+// #ifndef LOCAL
+// #include "debug.h"
+// #else
+// #define dg(x...)
+// #endif
+
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define allr(v) v.begin(), v.end(),greater<>()
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+
+  
+
+void mine(){
+
+    float n,k; cin>>n>>k;
+
+    vector<float>v(n);
+
+    forl(i,n)cin>>v[i];
+
+    float avg=0;
+
+    forl(i,n){
+        avg+=v[i];
+    }
+
+    avg/=n;
+
+
+    if(avg==k)pyes
+    else pno
+
+
+}
+
+
+
+int main() {
+    int t=1;
+    cin>>t;
+   
+    while(t--){
+        mine();
+    }
+    return 0;
+}
