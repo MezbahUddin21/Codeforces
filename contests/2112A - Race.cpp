1baf4fa211bdbc66f15805aec60edfa932821d9a
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-06-23 21:16:52 +0600

    Solve 2112A - Race [800]

---
+++ contests/2112A - Race.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+ 
+ 
+void mine(){
+    int a,x,y; cin>>a>>x>>y;
+
+    bool ok=0;
+
+    for(int i=-110; i<=110; i++){
+            if(a!=i){
+                if(abs(i-x)< abs(x-a)){
+                    if(abs(i-y)< abs(y-a)){
+                        pyes
+                        return;
+                    }
+                }
+            }
+    }
+
+    pno
+
+}
+ 
+int main() {
+    fast;
+    int t = 1; 
+    cin>>t;
+    // int c=1;
+    while (t--) {
+
+        // cout<<"Case "<<c<<":"<<endl; c++;
+        mine();
+    }
+    return 0;
+}
