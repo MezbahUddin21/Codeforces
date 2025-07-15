eb1dbe0725a36343d3dd89bf86f80da4ed11c2eb
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-07-15 12:30:07 +0600

    Solve 2092B - Lady Bug [1000]

---
+++ contests/2092B - Lady Bug.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long int
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+void mine(){
+    
+    int n; cin>>n;
+
+    string a,b; cin>>a>>b;
+    
+    int onea=0;
+    int oneb=0;
+
+    for(int i=0; i<n; i++){
+
+        if(i%2==0){
+            if(a[i]=='1')onea++;
+            if(b[i]=='1')oneb++;
+        }else{
+            if(a[i]=='1')oneb++;
+            if(b[i]=='1')onea++;
+        }
+
+    }
+
+    if(onea<=n/2 and oneb<=(n+1)/2)pyes
+    else pno
+
+    
+}
+
+
+int main(){
+    fast;
+    ll t = 1; 
+    cin>>t;
+    // int c=1;
+    while (t--){
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
+
