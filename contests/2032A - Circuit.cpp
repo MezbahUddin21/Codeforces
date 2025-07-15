60919007780ec05df1c928ff65400436b0a58c84
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-07-15 16:44:11 +0600

    Solve 2032A - Circuit [800]

---
+++ contests/2032A - Circuit.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+
+
+
+
+void mine(){
+
+    int n; cin>>n;
+
+    n*=2;
+    vector<int>v(n);
+
+    for(int i=0; i<n; i++)cin>>v[i];
+
+
+    int one=0;
+    int zero=0;
+
+    forl(i,n){
+        if(v[i]==0)zero++;
+        else one++;
+    }
+
+
+
+    int mn=(one%2)?1:0;
+
+    int mx=min(one,zero);
+
+    cout<<mn<<space<<mx<<endl;
+
+
+
+}
+
+
+
+
+ 
+signed main() {
+    ios::sync_with_stdio(0),cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+
+//    int c=1;
+    while(t--){
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
+
+
