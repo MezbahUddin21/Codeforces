5ba57ecea886edd55aa30b0a561e7f0392499a49
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-07-19 11:36:59 +0600

    Solve 797A - k-Factorization [1100]

---
+++ contests/797A - k-Factorization.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long int
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+ 
+void mine(){
+    
+    ll n,k; cin>>n>>k;
+
+
+    vector<ll>ans;
+
+    bool f=0;
+
+    for(ll i=2; i<=n; i++){
+        while(n%i==0){
+
+            if(ans.size()==k-1){
+                f=1;
+                break;
+            }
+            n/=i;
+            ans.pb(i);
+        }
+
+        if(f)break;
+    }
+
+    if(n>1)ans.pb(n);
+
+    if(ans.size()>=k){
+        for(auto it:ans)cout<<it<<space;
+    }else cout<<-1;
+
+
+}
+ 
+int main() {
+    fast;
+
+    int t = 1; 
+    // cin>>t;
+    // int c=1;
+    while (t--) {
+
+        // cout<<"Case "<<c<<":"<<endl; c++;
+        mine();
+    }
+    return 0;
+}
