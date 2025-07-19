1d500424fd25b4439d078e5452c44265ccafa31e
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-07-19 11:31:11 +0600

    Improve 230B - T-primes [1300]

--- contests/230B - T-primes.cpp
+++ contests/230B - T-primes.cpp
@@ -10,59 +10,66 @@ using namespace std;
 #define pno cout<<"NO"<<"\n";
 #define sortv sort(v.begin(),v.end())
 #define sortvg sort(v.begin(),v.end(),greater<>())
-#define int long long
+#define ll long long
 #define forl(i,n) for(int i=0;i<n;i++)
 #define for1(i,n) for(int i=1;i<=n;i++)
 #define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 
-const int mxN=1e6+5;
-map<int,int>primes;
- 
-void sieve() {
-    vector<bool> isPrime(mxN, true);
-    isPrime[0] = isPrime[1] = false;
- 
-    for (int i = 2; i < mxN; ++i) {
-        if (isPrime[i]) {
-            primes[i]=1;
-            for (int j = i + i; j < mxN; j += i) {
-                isPrime[j] = false;
+const int mx = 1e7+10;
+vector<bool>isPrime(mx);
+set<ll>primes;
+
+
+
+
+void sieve(int n){
+    for(int i=3; i<=n; i+=2)isPrime[i]=1;
+    int sq=sqrt(n);
+    for (int i=3; i<=sq; i+=2){
+        if(isPrime[i]){
+            for (int j =i*i; j<=n; j+=i+i){
+                isPrime[j]=0;
             }
         }
     }
-}
-
-
-
-void mezbah(){
-    int n; cin>>n;
-    while(n--){
-        int x; cin>>x;
-
-        if(x==4){
-            pyes
-            continue;
+    primes.insert(2);
+    for (int i=3; i<=n; i+=2 ) {
+        if(isPrime[i]==1) {
+            primes.insert(i);
         }
-        int sq=sqrt(x);
-
-        if(sq*sq==x  and primes[sq]==1){
-            pyes
-        }else pno
     }
 }
 
-
-
  
-signed main() {
-    // int t;
-    // cin>>t;
-    sieve();
-   
-    // while(t--){
-        mezbah();
-    // }
+void mine(){
+    ll n; cin>>n;
+
+    ll sq=sqrt(n);
+
+    if(sq*sq==n and primes.count(sq))pyes
+    else pno
+
+
+
+}
+ 
+int main() {
+    fast;
+
+    sieve(mx);
+
+    // forl(i,100)cout<<primes[i]<<space;
+    // cout<<endl;
+
+    int t = 1; 
+    cin>>t;
+    // int c=1;
+    while (t--) {
+
+        // cout<<"Case "<<c<<":"<<endl; c++;
+        mine();
+    }
     return 0;
 }
-
-
