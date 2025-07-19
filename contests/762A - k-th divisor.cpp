6d83db083a2e926103a6eed8f96f2db4c21f57ae
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-07-19 11:35:35 +0600

    Solve 762A - k-th divisor [1400]

---
+++ contests/762A - k-th divisor.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+ 
+void mine(){
+    
+    ll n,k; cin>>n>>k;
+
+    vector<ll>ans;
+
+    for(ll i=1; i*i<=n; i++){
+        if(n%i==0){
+            ans.pb(i);
+            if(n/i!=i)ans.pb(n/i);
+
+        }
+
+    }
+
+    sort all(ans);
+
+    if(ans.size()>=k)cout<<ans[k-1]<<endl;
+    else cout<<-1<<endl;
+
+}
+ 
+int main() {
+    fast;
+    int t = 1; 
+    // cin>>t;
+    // int c=1;
+    while (t--) {
+
+        // cout<<"Case "<<c<<":"<<endl; c++;
+        mine();
+    }
+    return 0;
+}
