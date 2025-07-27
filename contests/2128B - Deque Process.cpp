e4e0e02c7e9a3d6fccc1b7bdcc26bfea9bb38cd2
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-07-27 21:43:22 +0600

    Solve 2128B - Deque Process [1100]

---
+++ contests/2128B - Deque Process.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000003
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+ 
+void mine(){
+
+    int n; cin>>n;
+
+    vector<int>v(n);
+
+    forl(i,n)cin>>v[i];
+
+    vector<char>ans;
+
+
+
+    int prev=-1;
+
+    bool f=1;
+
+    for(int i=0; i<n/2; i++){
+
+        if(f){
+            if(v[i]<=v[n-i-1]){
+                ans.pb('L');
+                ans.pb('R');
+            }else{
+                ans.pb('R');
+                ans.pb('L');
+            }
+            f=0;
+        }else{
+            if(v[i]<=v[n-i-1]){
+                ans.pb('R');
+                ans.pb('L');
+            }else{
+                ans.pb('L');
+                ans.pb('R');
+            }
+            f=1;
+        }
+
+        
+    }
+
+    if(n%2){
+        ans.pb('L');
+    }
+
+
+    // cout<<ans.size()<<endl;
+    // // // cout<<ans.size()<<endl;
+
+    forl(i,n)cout<<ans[i];
+    cout<<endl;
+}
+ 
+int main() {
+    fast;
+    
+    ll t = 1; 
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
