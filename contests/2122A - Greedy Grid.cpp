d1b1b1bf41eb76860e5735ed21ca279accd5615d
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-08-06 23:48:34 +0600

    Solve 2122A - Greedy Grid [800]

---
+++ contests/2122A - Greedy Grid.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+void mine(){   
+
+    int n,m; cin>>n>>m;
+
+    if(n==1 or m==1 or (n<=2 and m<=2))pno
+    else pyes
+    
+    
+
+}
+
+
+
+int main() {
+    fast;
+    ll t = 1; 
+    cin>>t;
+    // cin.ignore();
+    // ll c=1;
+    while (t--) {    
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
+
+
+
