46cc06503a08469522b462d59859970f93b54393
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-08-19 20:31:51 +0600

    Solve 1818B - Indivisible [900]

---
+++ contests/1818B - Indivisible.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+
+
+void mine(){
+
+    int n; cin>>n;
+    if(n==1){
+        cout<<1<<endl;
+        return;
+    }
+    if((n*(n+1)/2)%n==0 and n!=1){
+        cout<<-1<<endl;
+        return;
+    }
+    
+    for(int i=n; i>=1; i-=2){
+        cout<<i-1<<space<<i<<space;
+    }cout<<endl;
+
+}
+
+int main(){
+    int t=1; 
+    cin>>t;
+    
+    // int c=1;
+    
+    while(t--){
+        
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+
+    return 0;
+}
