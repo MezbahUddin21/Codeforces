37ba4ebd7f703202ebe9c956346f98710a7414bf
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-09-08 21:01:50 +0600

    Solve 2139B - Cake Collection [800]

---
+++ contests/2139B - Cake Collection.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1e9+7
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"yes"<<"\n";
+#define pno cout<<"no"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+    n magical cakeovens.
+
+    ith bakes ai cakes 1sec
+
+    end of each sec teliport to any oven
+
+*/
+
+void mine(){
+
+    ll n,m; cin>>n>>m;
+
+    vector<ll>v(n);
+
+    forl(i,n){
+        cin>>v[i];
+    }
+
+    sort allr(v);
+
+
+
+    ll ans=0;
+    ll cnt=m;
+    forl(i,n){
+        ans+=v[i]*m;
+        m--;
+        if(m==0)break;
+    }
+
+    cout<<ans<<endl;
+
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
