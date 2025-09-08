86f1c7323af7db65a9af58959831b15f3e023fec
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-09-08 20:44:44 +0600

    Solve 2139A - Maple and Multiplication [800]

---
+++ contests/2139A - Maple and Multiplication.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1e9+7
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"yes"<<"\n";
+#define pno cout<<"no"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+void mine(){
+
+    int a,b; cin>>a>>b;
+
+
+    int ans=a*b/__gcd(a,b);
+
+    if(a==b)cout<<0<<endl;
+    else if(ans==a or ans==b)cout<<1<<endl;
+    else cout<<2<<endl;
+
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
