e41ddeb0b75c1fb0584c1558e99771138da6afb0
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-10-17 21:35:15 +0600

    Solve 2162C - Beautiful XOR [1100]

---
+++ contests/2162C - Beautiful XOR.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+void mine(){
+
+    ll a,b; cin>>a>>b;
+
+
+    vector<int>ans;
+
+
+    if(a==b){
+        cout<<0<<endl;
+    }else{
+        
+
+        int aa,bb;
+
+        aa=bb=-1;
+
+        forl(i,31+1){
+
+            if(((a>>i)&1) != ((b>>i)&1)){
+                int num=(1<<i);
+                ans.pb(num);
+
+            }
+
+            if((a>>i)&1)aa=max(aa,i);
+            if((b>>i)&1)bb=max(bb,i);
+
+
+        }
+
+        if(aa<bb){
+            cout<<-1<<endl;
+            return;
+        }
+
+        cout<<ans.size()<<endl;
+
+        for(auto it:ans)cout<<it<<space;
+
+        cout<<endl;
+
+    }
+
+
+
+
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
