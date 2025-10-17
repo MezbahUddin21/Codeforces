4053d85d178eaadaf057a1baafec47c1571e5fb2
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-10-17 20:54:11 +0600

    Solve 2162B - Beautiful String [1000]

---
+++ contests/2162B - Beautiful String.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+void mine(){
+
+    int n; cin>>n;
+
+    string s; cin>>s;
+
+    int cnt1=0;
+    int cnt0=0;
+
+    vector<int>ans;
+
+    forl(i,n){
+        if(s[i]=='1'){
+            cnt1++;
+            ans.pb(i+1);
+        }
+    }
+
+    if(cnt1==n or cnt1==0){
+
+        cout<<0<<endl;
+        return;
+    }
+
+    cout<<ans.size()<<endl;
+
+    for(auto it:ans)cout<<it<<space;
+
+    cout<<endl;
+
+
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
