a239db24d71ca04ff52f06817d23d3aac9e2a64d
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-11-07 09:45:18 +0600

    Solve 1269A - Equation [800]

---
+++ contests/1269A - Equation.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+// #define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long int
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+/*
+
+
+
+
+*/
+
+void mine(){
+
+    ll n; cin>>n;
+
+    if(n==1){
+
+        cout<<9<<space<<8<<endl;
+        return;
+
+    }
+
+
+    cout<<3LL*n<<space<<2LL*n<<endl;
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1; 
+    // cin>>t;
+    // int c=1;
+    while (t--){
+        // cout<<"Case "<<c<<":"<<endl; c++;
+        mine();
+    }
+    return 0;
+}
