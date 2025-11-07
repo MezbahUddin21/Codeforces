2e272af97f39c4a4e87d0581062d30ca6ed83342
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-11-07 10:31:25 +0600

    Solve 205B - Little Elephant and Sorting [1400]

---
+++ contests/205B - Little Elephant and Sorting.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+// #define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long int
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+/*
+
+
+
+
+*/
+
+void mine(){
+
+
+    ll n; cin>>n;
+
+    vector<ll>v(n);
+
+    forl(i,n)cin>>v[i];
+
+    ll ans=0;
+
+    ll prev=v[0],mn=2LL*INT_MAX;
+    for(int x=1; x<n; x++){
+        if(v[x]<v[x-1]){
+            mn=min(mn,v[x]);
+        }
+        else{
+            if(mn!=2LL*INT_MAX){
+                ans+=prev-mn;
+                mn=2LL*INT_MAX;
+                prev=v[x];
+            }
+        }
+        prev=max(v[x],prev);
+        // cout << prev << " " << mn << " " << ans << endl;
+    }
+    if(mn!=2LL*INT_MAX) ans+=prev-mn;
+
+
+
+
+
+    cout<<ans<<endl;
+
+}
+
+int main() {
+    fast;
+    ll t = 1; 
+    // cin>>t;
+    // int c=1;
+    while (t--){
+        // cout<<"Case "<<c<<":"<<endl; c++;
+        mine();
+    }
+    return 0;
+}
