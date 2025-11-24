a97f69895a7c991317770f8e44a343976951be58
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-11-24 13:01:20 +0600

    Solve 2171C1 - Renako Amaori and XOR Game (easy version) [1100]

---
+++ contests/2171C1 - Renako Amaori and XOR Game (easy version).cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+void mine(){
+
+    int n; cin>>n;
+
+    vector<int>a(n);
+    vector<int>b(n);
+
+    forl(i,n)cin>>a[i];
+    forl(i,n)cin>>b[i];
+
+    int x1=0,x2=0;
+
+    forl(i,n)x1^=a[i];
+    forl(i,n)x1^=b[i];
+    
+    if(x1==x2){
+        cout<<"Tie"<<endl;
+        return;
+    }
+
+    int f=-2;
+
+    forl(i,n){
+        if(a[i]^b[i]&1){
+            f=i;
+        }
+    }
+
+    if(f&1)cout<<"Mai"<<endl;
+    else cout<<"Ajisai"<<endl;
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
