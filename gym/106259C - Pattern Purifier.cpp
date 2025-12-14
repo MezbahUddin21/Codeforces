bb7bac5c052cc0f11453fcd885184b2da0b7c1d1
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-12-14 13:56:17 +0600

    Solve 106259C - Pattern Purifier

---
+++ gym/106259C - Pattern Purifier.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+void mine(){
+
+    string s; cin>>s;
+    int n=s.size();
+    stack<char>st;
+
+    forl(i,n){
+        if(!st.empty() and st.top()==s[i]){
+            st.pop();
+        }else st.push(s[i]);
+    }
+
+
+    // cout<<st.size()<<endl;
+
+    if(st.empty())pyes
+    else pno
+
+
+
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
