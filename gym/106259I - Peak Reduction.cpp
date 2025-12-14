61ec40341400ab59c9d403c38f7e4eeb48e1830d
Mezbah <mezbahuddinmaruf12345@gmail.com>
2025-12-14 13:38:03 +0600

    Solve 106259I - Peak Reduction

---
+++ gym/106259I - Peak Reduction.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+void mine(){
+
+    
+    ll n; cin>>n;
+
+    vector<int>v(n);
+
+    forl(i,n)cin>>v[i];
+
+    
+    
+    if((v[0]==1 or v[0]==2) and (v[n-1]==1 or v[n-1]==2))pyes
+    else pno
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
