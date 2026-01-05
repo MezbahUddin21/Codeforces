a61d9481871828336d275f34c4dc8761fb628e06
Mezbah <mezbahuddinmaruf12345@gmail.com>
2026-01-06 01:17:23 +0600

    Solve 2146A - Equal Occurrences [800]

---
+++ contests/2146A - Equal Occurrences.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+
+
+void mine(){
+
+    ll n; cin>>n;
+
+    vector<ll>cnt(1000,0);
+
+    forl(i,n){
+        ll x; cin>>x;
+
+        cnt[x]++;
+    }
+
+    int ans=0;
+
+    for1(i,100){
+        int c=0;
+
+        for(auto it:cnt){
+            if(it>=i)c+=i;
+        }
+        ans=max(ans,c);
+
+    }
+
+    cout<<ans<<endl;
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
