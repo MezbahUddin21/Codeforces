1cb0ed7dc4ecbbc74427c4f7029057484977d9de
Mezbah <mezbahuddinmaruf12345@gmail.com>
2026-01-11 23:59:05 +0600

    Solve 2181F - Fragmented Nim [1600]

---
+++ contests/2181F - Fragmented Nim.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+void mine(){
+
+    int n; cin>>n;
+
+    vector<int>v(n);
+
+    int cnt_one=0;
+
+    bool flag=0;
+
+    forl(i,n){
+        cin>>v[i];
+        if(v[i]==1)cnt_one++;
+        if(v[i]>1)flag=1;
+    }   
+
+    if((cnt_one+flag)%2)cout<<"Alice"<<endl;
+    else cout<<"Bob"<<endl;
+
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
