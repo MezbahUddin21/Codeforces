c3b3ad1daf26deafcae1f77bb9f4a955855bbe7e
Mezbah <mezbahuddinmaruf12345@gmail.com>
2026-01-18 12:17:56 +0600

    Solve 2182A - New Year String [800]

---
+++ contests/2182A - New Year String.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+void mine(){
+
+    int n; cin>>n;
+
+    string s; cin>>s;
+
+    string s1="2025";
+    string s2="2026";
+
+    size_t check1=s.find(s2);
+    size_t check2=s.find(s1);
+
+
+    if(check1!=string::npos){
+        cout<<0<<endl;
+        return;
+    }else if(check2!=string::npos){
+        cout<<1<<endl;
+        return;
+    }else cout<<0<<endl;
+
+
+
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
