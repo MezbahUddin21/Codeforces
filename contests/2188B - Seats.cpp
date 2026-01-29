42956f7bd77a8d2e0cd0a767eb446a92419bce6c
Mezbah <mezbahuddinmaruf12345@gmail.com>
2026-01-29 21:06:53 +0600

    Solve 2188B - Seats [1000]

---
+++ contests/2188B - Seats.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+    row of n seats
+    forbidden next to each other
+    s
+
+*/
+
+void mine(){
+
+    int n; cin>>n;
+
+    string s; cin>>s;
+
+
+    s="0"+s+"0";
+
+    // cout<<s<<endl;
+
+    n+=2;
+
+    int cnt=0;
+    int one=0;
+
+    for(int i=0; i<n; i++){
+        if(s[i]=='0')cnt++;
+        else{
+            if(cnt==3){
+                one++;
+            }
+            cnt=0;
+            one++;
+        }
+
+        if(cnt==3){
+            one++;
+            cnt=0;
+        }
+    }
+    
+    cout<<one<<endl;
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
