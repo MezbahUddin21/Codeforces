5422a06c0be2a02bfaab6e42bf67cd3e1096f500
Mezbah <mezbahuddinmaruf12345@gmail.com>
2026-03-11 12:24:12 +0600

    Solve 2180B - Ashmal [800]

---
+++ contests/2180B - Ashmal.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+void mine(){
+
+    int n; cin>>n;
+
+    string s="";
+
+    forl(i,n){
+
+        string x; cin>>x;
+
+        string s1=x+s;
+        string s2=s+x;
+
+        s=min(s1,s2);
+    }
+
+    cout<<s<<endl;
+
+
+
+    // string s1="bbbaaaa";
+    // string s2="bbb";
+
+    // if(s2>s1){
+    //     swap(s1,s2);
+    // }
+
+
+    // cout<<s1<<space<<s2<<endl;
+
+
+}
+
+int main() {
+    fast;
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
