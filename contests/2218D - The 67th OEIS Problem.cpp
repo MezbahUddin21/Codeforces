41f81ed0b01eb787d7369956df92b2c5d1d106e1
Mezbah <mezbahuddinmaruf12345@gmail.com>
2026-04-04 22:02:08 +0600

    Solve 2218D - The 67th OEIS Problem [1100]

---
+++ contests/2218D - The 67th OEIS Problem.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1000000007
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"YES"<<"\n";
+#define pno cout<<"NO"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+/*
+
+
+*/
+
+
+vector<ll>primes;
+
+void sieve(ll n){
+    vector<bool>isP(n+1,true);
+    isP[0]=isP[1]=false;
+    
+    for(ll i=2;i<=n;i++){
+        if(isP[i]){
+            primes.pb(i);
+            for(ll j=i*i;j<=n;j+=i)
+                isP[j]=false;
+        }
+    }
+}
+
+
+void mine(){
+
+    ll n; cin>>n;
+
+    vector<ll>v(n);
+
+
+    forl(i,n){
+        v[i]=primes[i]*primes[i+1];
+    }
+
+
+    // vector<ll>pp;
+
+    // forl(i,n-1){
+    //     pp.pb(__gcd(v[i],v[i+1]));
+    // }
+
+    // sort all(pp);
+
+
+    for(auto it:v)cout<<it<<space;
+
+    cout<<endl;
+
+
+
+
+
+}
+
+int main() {
+    fast;
+    sieve(1000000);
+    ll t = 1;
+    cin>>t;
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
