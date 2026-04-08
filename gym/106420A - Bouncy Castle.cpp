d0bde4b0f3e51ea1e812d8b4eeaf650c44dab0e5
Mezbah <mezbahuddinmaruf12345@gmail.com>
2026-04-08 14:56:42 +0600

    Solve 106420A - Bouncy Castle

---
+++ gym/106420A - Bouncy Castle.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define mod 1e9+7
+#define pb push_back
+#define all(v) (v.begin(), v.end())
+#define allr(v) (v.begin(), v.end(),greater<>())
+#define pyes cout<<"yes"<<"\n";
+#define pno cout<<"no"<<"\n";
+#define sortv sort(v.begin(),v.end())
+#define sortvg sort(v.begin(),v.end(),greater<>())
+#define ll long long
+#define forl(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define setprecision(x) cout << fixed << setprecision(x)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+ 
+void mine(){
+ 
+
+ 	// int n,m; cin>>n>>m;
+
+ 	// vector<vector<int>>g(n+1);
+
+
+ 	// forl(i,m){
+ 	// 	int x,y; cin>>x>>y;
+
+
+ 	// }
+
+
+	ll n; cin>>n;
+
+ 	ll ans=sqrt(2*1LL*n);
+
+ 	cout<<ans<<endl;
+
+}
+
+
+
+int main() {
+    fast;
+
+    ll t = 1;
+    // cin>>t
+    // ll c=1;
+    while (t--) {
+        // cout<<"Case "<<c<<": "; c++;
+        mine();
+    }
+    return 0;
+}
