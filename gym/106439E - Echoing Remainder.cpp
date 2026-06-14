ad730079e84d6680fbbc9bac320855fb7f2a2bf9
Mezbah <mezbahuddinmaruf12345@gmail.com>
2026-06-14 12:03:45 +0600

    Solve 106439E - Echoing Remainder

---
+++ gym/106439E - Echoing Remainder.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+
+void solve(){
+
+
+    int n; 
+
+    cin>>n;
+
+    cout<<1<<" ";
+
+    for(int i=n; i>1; i--){
+        cout<<i<<" ";
+    }
+
+    cout<<endl;
+
+
+}
+
+int main(){
+
+
+
+    int t; cin>>t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
