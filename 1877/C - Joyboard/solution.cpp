35baeacdfe2c37aa1e9aee805d5535afa55829c0
Mezbah Uddin Maruf <78334304+MezbahUddin21@users.noreply.github.com>
2026-07-13 16:12:56 +0600

    Add Joyboard [C] from Codeforces

---
+++ 1877/C - Joyboard/solution.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+ 
+ 
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+ 
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+ 
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+ 
+ 
+ 
+ 
+void solve(){
+        int n,m,k;
+        cin>>n>>m>>k;
+        if(k==1) cout<<"1"<<endl;
+        else if(k==2){
+             if(n > m)
+             {
+                 cout<<m<<endl;
+             }
+             else
+             {
+                 cout<<(n-1)+m/n<<endl;
+             }
+        }
+        else if(k==3)
+        {
+            if(m > n){
+                int ans = m - n + 1;
+                ans -= m/n;
+                cout<<ans<<endl;
+            }
+            else cout<<"0"<<endl;
+        }
+        else cout<<"0"<<endl;
+}
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+ 
+ 
+ 
+ 
