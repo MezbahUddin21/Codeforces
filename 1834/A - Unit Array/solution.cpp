af74eb303f5f6aa14fe2767e8b84749f19d21d1f
Mezbah Uddin Maruf <78334304+MezbahUddin21@users.noreply.github.com>
2026-07-13 16:11:54 +0600

    Add Unit Array [A] from Codeforces

---
+++ 1834/A - Unit Array/solution.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+ 
+ 
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+ 
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+ 
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+ 
+void solve(){
+ 
+	int n; cin>>n;
+	int x;
+    int cnt=0,ans=0;
+	for(int i=0; i<n; i++){
+        cin>>x;
+        if(x==-1)cnt++;
+    }
+ 
+    if(cnt>(n-cnt)){
+        int f=(cnt-(n-cnt)+1)/2;
+        cnt-=f;
+        ans+=f;
+    }
+    if(cnt%2==1){
+        ans+=1;
+    }
+    cout<<ans<<endl;
+ 
+ 
+	
+}
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+ 
+ 
+ 
+ 
