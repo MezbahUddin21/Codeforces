2d82732e944aab224437b086c18fba8328e04fa6
Mezbah Uddin Maruf <78334304+MezbahUddin21@users.noreply.github.com>
2026-07-22 09:48:37 +0600

    Add Distinct Buttons [A] from Codeforces

---
+++ 1909/A - Distinct Buttons/solution.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define setprecision(x) cout << fixed << setprecision(x)
+void mezbah(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+ 
+ 
+void seive(int n){
+    vector<int>v(n+3);
+    for(int i=2; i*i<=n; i++){
+        if(v[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                v[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(v[i]==0)cout<<i<<" ";
+    }
+    cout<<endl;
+}
+ 
+vector<ll> mPrimeFact(ll x) {
+	vector<ll> fact;
+	for(ll i = 2; i * i <= x; i++) {
+		ll cnt = 0; 
+		while(x % i == 0) {
+			cnt++;
+			x /= i;
+			fact.push_back(i);
+		}
+	}
+	if(x > 1) fact.push_back(x);
+	return fact;
+}
+ 
+ll lcm(ll a,ll b){
+    ll g=__gcd(a,b);
+    return (a*b/g);
+}
+ 
+ 
+ 
+ 
+void solve(){
+    int n; cin>>n;
+            int u=0,r=0,l=0,d=0;
+    while(n--){
+ 
+        int x,y; cin>>x>>y;
+        if(x>0)u=1;
+        if(y>0)r=1;
+        if(x<0)l=1;
+        if(y<0)d=1;
+ 
+    }
+            if(u+r+l+d==4){
+            cout<<"no"<<endl;
+        }else cout<<"yes"<<endl;
+}
+ 
+ 
+int main() {
+    mezbah();
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+ 
+ 
+ 
+ 
